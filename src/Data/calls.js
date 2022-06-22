e9ec0a8106e43ca9e83323ac7700f7f1529872f4
Anushka Dubey <71165722+anushkadubey@users.noreply.github.com>
2022-06-22 20:42:35 +0530

    Update calls.js

--- src/Data/calls.js
+++ src/Data/calls.js
@@ -2,24 +2,24 @@ export const CallsMenu=[
     {
         id:1,
         name:"SpeedDial",
-        icon:<i className="fi-rr-smartphone"></i>,
+        icon:<i className=" fi fi-rr-smartphone"></i>,
     },
     {
         id:2,
         name:"Contacts",
-        icon:<i className="fi-rr-portrait"></i>,
+        icon:<i className="fi fi-rr-portrait"></i>,
     },
 
 {
     id:3,
     name:"History",
-    icon:<i className="fi-rr-time-past"></i>,
+    icon:<i className="fi fi-rr-time-past"></i>,
 },
 
 {
     id:1,
     name:"Voicemail",
-    icon:<i className="fi-rr-microphone"></i>,
+    icon:<i className=" fi fi-rr-microphone"></i>,
 },
 ];
 
@@ -28,20 +28,20 @@ export const  DialerList =[
         id:1,
         name:"Bhavya Shukla",
         type:"Student", 
-        avatar:"https://cdn.icon-icons.com/icons2/1879/PNG/128/iconfinder-11-avatar-2754576_120520.png", 
+        avatar:"https://cdn.icon-icons.com/icons2/582/PNG/512/woman_icon-icons.com_55031.png", 
     },
     {
         id:2,
         name:"Bharti Singh",
         type:"Teacher", 
-        avatar:"https://image.flaticon.com/icons/png/128/219/219969.png",
+        avatar:"https://cdn.icon-icons.com/icons2/884/PNG/512/person_9_icon-icons.com_68901.png",
     },
 
 {
     id:3,
-    name:"Athar Hussain",
+    name:"Chinmai Dubey",
     type:"Admin", 
-    avatar:"https://image.flaticon.com/icons/png/128/2922/2922510.png"
+    avatar:"https://cdn.icon-icons.com/icons2/546/PNG/512/1455555018_users-2_icon-icons.com_53266.png"
 }
 ]
 
@@ -56,30 +56,30 @@ export const CallsContact=[
         id:2,
         name:"Soham Gupta",
         type: "Student",
-        avatar:"https://cdn.icon-icons.com/icons2/1879/PNG/128/iconfinder-11-avatar-2754576_120520.png"
+        avatar:"https://cdn.icon-icons.com/icons2/2643/PNG/512/female_woman_avatar_people_person_white_tone_icon_159370.png"
     },
     {
         id:1,
         name:"Ayushi Sinha",
         type: "Student",
-        avatar:"https://cdn.icon-icons.com/icons2/1879/PNG/128/iconfinder-11-avatar-2754576_120520.png"
+        avatar:"https://cdn.icon-icons.com/icons2/1999/PNG/512/avatar_people_person_profile_user_woman_icon_123359.png"
     },
     {
         id:2,
         name:"Riya",
         type: "Student",
-        avatar:"https://cdn.icon-icons.com/icons2/1879/PNG/128/iconfinder-11-avatar-2754576_120520.png"
+        avatar:"https://cdn.icon-icons.com/icons2/1465/PNG/512/156womanofficeworker2_100687.png"
     },
     {
         id:1,
         name:"Nidhi Bathwal",
         type: "Student",
-        avatar:"https://cdn.icon-icons.com/icons2/1879/PNG/128/iconfinder-11-avatar-2754576_120520.png"
+        avatar:"https://cdn.icon-icons.com/icons2/582/PNG/512/girl_icon-icons.com_55043.png"
     },
     {
         id:2,
         name:"Muskan Pathak",
         type: "Teacher",
-        avatar:"https://cdn.icon-icons.com/icons2/1879/PNG/128/iconfinder-11-avatar-2754576_120520.png"
+        avatar:"https://cdn.icon-icons.com/icons2/2228/PNG/512/mask_wearing_avatar_woman_covid_coronavirus_air_pollution_icon_134540.png"
     },
 ]
